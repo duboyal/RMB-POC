a00a1f95aeb95ac5dfe38548e89dd726c3173bd1
Ali <duboyal2@gmail.com>
2026-04-13 16:41:27 -0400

    env configuration

--- app/db.py
+++ app/db.py
@@ -1,3 +1,10 @@
+# from sqlalchemy import create_engine
+# engine = create_engine("postgresql+psycopg2://user:pass@postgres:5432/rmb")
+
+import os
 from sqlalchemy import create_engine
 
-engine = create_engine("postgresql+psycopg2://user:pass@postgres:5432/rmb")
+engine = create_engine(
+    f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+)
