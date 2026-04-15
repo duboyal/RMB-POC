e7a1901081c76d9036f9eb3e6ed94512faad9e26
Ali <duboyal2@gmail.com>
2026-04-15 17:12:26 -0400

    change table name to see difference

--- app/importer.py
+++ app/importer.py
@@ -5,6 +5,6 @@ from db import engine
 def import_file(path):
     df = pd.read_csv(path)
 
-    df.to_sql("order_header", con=engine, if_exists="append", index=False)
+    df.to_sql("order_header_222", con=engine, if_exists="append", index=False)
 
     return len(df)
