ff0c3db2c6326317056d464c6d0a4c69b34a6995
Ali <duboyal2@gmail.com>
2026-04-16 11:55:11 -0400

    adding dynamic table naming

--- app/importer.py
+++ app/importer.py
@@ -1,10 +1,23 @@
 import pandas as pd
 from db import engine
+import re
 
 
 def import_file(path):
+
     df = pd.read_csv(path)
 
-    df.to_sql("order_header_222", con=engine, if_exists="append", index=False)
+    # sanitize table name
+    table_name = re.sub(r"[^a-zA-Z0-9_]", "_", path.stem).lower()
+
+    print(f"Importing into table: {table_name}")
+
+    df.to_sql(table_name, con=engine, if_exists="append", index=False)
 
     return len(df)
+
+
+# def import_file(path):
+#     df = pd.read_csv(path)
+#     df.to_sql("order_header", con=engine, if_exists="append", index=False)
+#     return len(df)
